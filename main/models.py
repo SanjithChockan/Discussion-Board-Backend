01dcdb7ab631e4b456fb4e7fe50074874435c779
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-04-10 18:04:50 -0500

    refactored, beautified, , fixed all url get parameters, and added optional parameters to all get functions

--- main/models.py
+++ main/models.py
@@ -1,14 +1,14 @@
 class Answer:
-    def __init__(self, post_id, user_id, content, time_created):
-        self.answer_id = -1
+    def __init__(self, post_id, user_id, content, time_created, answer_id =-1):
+        self.answer_id = answer_id
         self.post_id = post_id
         self.user_id = user_id
         self.content = content
         self.time_created = time_created
 
 class Post:
-    def __init__(self, user_id, course_id, title, content, time_created, answer_count):
-        self.post_id = -1
+    def __init__(self, user_id, course_id, title, content, time_created, answer_count, post_id=-1):
+        self.post_id = post_id
         self.user_id = user_id
         self.course_id = course_id
         self.title = title
--- main/routes/read.py
+++ main/routes/read.py
@@ -4,98 +4,109 @@ from util import related_post_and_search
 from datetime import datetime
 read_bp = Blueprint('read', __name__)
 from app import db
+DEFAULT_N = 50
 
 
 # Get all posts from db
 @read_bp.route('/get_all_posts', methods=['GET'])
-def get_all_posts():
-    #data = request.get_json()
-    n = 1000 #data['n']
+@read_bp.route('/get_all_posts/<int:n>', methods=['GET'])
+def get_all_posts(n=DEFAULT_N):
+    query = f"SELECT * FROM posts LIMIT {n};"
 
     cur = db.cursor()
-    query = f"SELECT * FROM posts LIMIT {n};"
     cur.execute(query)
-    posts = cur.fetchall()
+    sql_posts = cur.fetchall()
+
+    posts = []
+    for row in sql_posts:
+        post = Post(row[1], row[2], row[3], row[4], row[5], row[6], row[0])
+        posts.append(post.__dict__)
+
     return jsonify(posts), 200
+
 
 # Get specific post
 @read_bp.route('/get_specific_post', methods=['GET'])
-def get_specific_post():
-    data = request.get_json()
-    post_id = data['post_id']
+@read_bp.route('/get_specific_post/<int:post_id>', methods=['GET'])
+def get_specific_post(post_id, n=DEFAULT_N):
+    query = f"SELECT * FROM posts WHERE post_id = {post_id} LIMIT {n}"
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM posts WHERE post_id = %s", (post_id,))
+    cur.execute(query)
     post = cur.fetchone()
+
     return jsonify(post), 200
 
+
 # Get related post(s):
-@read_bp.route('/get_related_posts', methods=['GET'])
-def get_related_posts():
-    data = request.get_json()
-    post_id = data['post_id']
-    n = data['n']
+@read_bp.route('/get_related_posts/<int:post_id>', methods=['GET'])
+@read_bp.route('/get_related_posts/<int:post_id>/<int:n>', methods=['GET'])
+def get_related_posts(post_id, n=DEFAULT_N):
+    related_post_ids = related_post_and_search.find_most_related_posts(post_id, n, db)
+
+    query = "SELECT * FROM posts WHERE post_id IN (" + ','.join(map(str, related_post_ids)) + f") LIMIT {n}"
 
     cur = db.cursor()
-    related_post_ids = related_post_and_search.find_most_related_posts(post_id, n, db)
-    # Format query and execute
-    query = "SELECT * FROM posts WHERE post_id IN (" + ','.join(map(str, related_post_ids)) + ")"
     cur.execute(query)
     posts = cur.fetchall()
+
     return jsonify(posts), 200
+
 
 # Lookup n related posts:
-@read_bp.route('/search/', methods=['GET'])
-def search():
-    data = request.get_json()
-    sentence = data['sentence']
-    n = data['n']
+@read_bp.route('/search/<string:query>', methods=['GET'])
+@read_bp.route('/search/<string:query>/<int:n>', methods=['GET'])
+def search(query, n=DEFAULT_N):
+    lookup_post_ids = related_post_and_search.lookup_related_posts(query, n, db)
 
-    cur = db.cursor()
-    lookup_post_ids = related_post_and_search.lookup_related_posts(sentence, n, db)
-    
-    # Format query and execute
     query = "SELECT * FROM posts WHERE post_id IN (" + ','.join(map(str, lookup_post_ids)) + ")"
+    
+    cur = db.cursor() 
     cur.execute(query)
     posts = cur.fetchall()
+
     return jsonify(posts), 200
+
 
 # Get user posts (My posts)
 @read_bp.route('/get_user_posts', methods=['GET'])
-def get_user_posts():
-    # implement current_user.id later when we actually have users
-    data = request.get_json()
-    user_id = data['user_id']
-    n = data['n']
+@read_bp.route('/get_user_posts/<int:n>', methods=['GET'])
+def get_user_posts(n=DEFAULT_N):
+    user_id = 3 # implement current_user.id later when we actually have users
 
-    cur = db.cursor()
     query = f'SELECT * FROM posts WHERE user_id = {user_id} LIMIT {n}'
+    
+    cur = db.cursor()
     cur.execute(query)
     posts = cur.fetchall()
+
     return jsonify(posts), 200
+
 
 # Get recommended posts
 @read_bp.route('/get_recommended_posts', methods=['GET'])
 def get_recommended_posts():
-    # data = request.get_json()
-    cur = db.cursor()
     query = "SELECT * FROM posts WHERE user_id = ? ()"
+
+    cur = db.cursor()
     cur.execute(query)
     posts = cur.fetchall()
+
     return jsonify(posts), 200
+
 
 # Get professor posts
-@read_bp.route('/get_professor_posts', methods=['GET'])
-def get_professor_posts():
-    data = request.get_json()
-    professor_id = data['professor_id']
-    n = data['n']
+@read_bp.route('/get_professor_posts/<int:professor_id>', methods=['GET'])
+@read_bp.route('/get_professor_posts/<int:professor_id>/<int:n>', methods=['GET'])
+def get_professor_posts(professor_id, n=DEFAULT_N):
+    query = f'SELECT * FROM posts WHERE user_id = {professor_id} LIMIT {n}'
 
     cur = db.cursor()
-    query = f'SELECT * FROM posts WHERE user_id = {professor_id} LIMIT {n}'
     cur.execute(query)
     posts = cur.fetchall()
+
     return jsonify(posts), 200
+
 
 # Get recent posts
 @read_bp.route('/get_recent_posts', methods=['GET'])
@@ -109,17 +120,18 @@ def get_recent_posts():
 
 
 # Get answers for specific post
-@read_bp.route('/get_answers_for_post', methods=['GET'])
-def get_answers_for_post():
-    data = request.get_json()
-    post_id = data['post_id']
+@read_bp.route('/get_answers_for_post/<int:post_id>', methods=['GET'])
+@read_bp.route('/get_answers_for_post/<int:post_id>/<int:n>', methods=['GET'])
+def get_answers_for_post(post_id, n=DEFAULT_N):
+    query = f"SELECT * FROM answers WHERE post_id = {post_id} LIMIT {n}"
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM answers WHERE post_id = %s", (post_id,))
+    cur.execute(query)
     sql_answers = cur.fetchall()
-    answers = {}
-    for i, row in enumerate(sql_answers):
-        answer = Answer(row[1], row[2], row[3], row[4])
-        answer.answer_id = row[0]
-        answers[i] = answer.__dict__
+
+    answers = []
+    for row in sql_answers:
+        answer = Answer(row[1], row[2], row[3], row[4], row[0])
+        answers.append(answer.__dict__)
+
     return jsonify(answers), 200
