bb7de92e75aa9e301e5f0180d5787ec885a8726b
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-04-13 13:53:37 -0500

    adding title score to search and related posts

--- util/related_post_and_search.py
+++ util/related_post_and_search.py
@@ -6,6 +6,32 @@ from typing import List
 threshold = 0.3
 
 
+def cosine_similarity(posts, index, text):
+    # Create a TfidfVectorizer to convert text to numerical vectors
+    vectorizer = TfidfVectorizer(stop_words=None)
+
+    # Create a list of all post contents and a dictionary mapping ids to indices
+    all_posts = [post[index] for post in posts]
+
+    # Create a matrix of all post contents
+    post_matrix = vectorizer.fit_transform(all_posts)
+
+    # Convert target post to numerical vector
+    target_vector = vectorizer.transform([text])
+
+    # Calculate cosine similarity between target post and all other posts
+    similarities = np.dot(post_matrix, target_vector.T).toarray().flatten()
+
+    # Sort similarities in descending order and get indices of top posts
+    top_indices = similarities.argsort()[::-1]
+
+    # Retrieve the ids of the top posts
+    scores = [[posts[i][0], similarities[i]] for i in top_indices]
+
+    # Return the ids of the top posts
+    return scores
+
+
 def find_most_related_posts(
     target_post_id: int, n: int, db: mysql.connector.connection.MySQLConnection
 ) -> List[int]:
@@ -14,80 +40,83 @@ def find_most_related_posts(
 
     # Retrieve the content and course ID of the target post from the MySQL database
     cursor = db.cursor()
-    query = "SELECT content, course_id FROM posts WHERE post_id = %s"
+    query = "SELECT title, content, course_id FROM posts WHERE post_id = %s"
     params = (target_post_id,)
     cursor.execute(query, params)
-    target_post_content, target_post_course_id = cursor.fetchone()
+    target_post_title, target_post_content, target_post_course_id = cursor.fetchone()
     cursor.close()
 
     # Retrieve all posts in the same course as the target post from the MySQL database
     cursor = db.cursor()
-    query = "SELECT post_id, content FROM posts WHERE course_id = %s AND post_id <> %s"
-    params = (target_post_course_id, target_post_id)
+    query = "SELECT post_id, title, content, course_id FROM posts WHERE course_id = %s AND post_id <> %s LIMIT %s"
+    params = (target_post_course_id, target_post_id, n)
     cursor.execute(query, params)
     posts = cursor.fetchall()
     cursor.close()
 
-    # Create a list of all post contents and a dictionary mapping ids to indices
-    all_posts = [post[1] for post in posts]
-    id_to_index = {post[0]: i for i, post in enumerate(posts)}
+    content_scores = cosine_similarity(posts, 2, target_post_content)
+    title_scores = cosine_similarity(posts, 1, target_post_title)
+    scale = 5
 
-    # Create a matrix of all post contents
-    post_matrix = vectorizer.fit_transform(all_posts)
+    combined_scores = {}
+    for id, score in content_scores:
+        if id not in combined_scores:
+            combined_scores[id] = score * 1
+        else:
+            combined_scores[id] += score * 1
+    for id, score in title_scores:
+        if id not in combined_scores:
+            combined_scores[id] = score * scale
+        else:
+            combined_scores[id] += score * scale
 
-    # Convert target post to numerical vector
-    target_vector = vectorizer.transform([target_post_content])
+    keys_to_delete = [
+        id for id in combined_scores if combined_scores[id] < (1 + 5) * threshold
+    ]
+    [combined_scores.pop(id) for id in keys_to_delete]
 
-    # Calculate cosine similarity between target post and all other posts
-    similarities = np.dot(post_matrix, target_vector.T).toarray().flatten()
+    # Sort dictionary by value (score) in descending order
+    sorted_scores = sorted(combined_scores.items(), key=lambda x: x[1], reverse=True)
 
-    # Sort similarities in descending order and get indices of top n posts
-    if n > len(posts):
-        top_n_indices = similarities.argsort()[::-1]
-    else:
-        top_n_indices = similarities.argsort()[::-1][:n]
-
-    # Retrieve the ids of the top n posts
-    top_n_ids = [posts[i][0] for i in top_n_indices if similarities[i] > threshold]
-
-    # Return the ids of the top n posts
-    return top_n_ids
+    # Extract sorted list of ids
+    sorted_ids = [item[0] for item in sorted_scores]
+    return sorted_ids
 
 
 def lookup_related_posts(
     search_sentence: str, n: int, db: mysql.connector.connection.MySQLConnection
 ) -> List[int]:
-    # Create a TfidfVectorizer to convert text to numerical vectors
-    vectorizer = TfidfVectorizer(stop_words=None)
-
     # Retrieve all posts from the MySQL database
     cursor = db.cursor()
-    query = "SELECT post_id, content FROM posts"
+    query = "SELECT post_id, title, content, course_id FROM posts"
     cursor.execute(query)
     posts = cursor.fetchall()
     cursor.close()
 
-    # Create a list of all post contents and a dictionary mapping ids to indices
-    all_posts = [post[1] for post in posts]
-    id_to_index = {post[0]: i for i, post in enumerate(posts)}
+    content_scores = cosine_similarity(posts, 2, search_sentence)
+    title_scores = cosine_similarity(posts, 1, search_sentence)
+    scale = 5
 
-    # Create a matrix of all post contents
-    post_matrix = vectorizer.fit_transform(all_posts)
+    combined_scores = {}
+    for id, score in content_scores:
+        if id not in combined_scores:
+            combined_scores[id] = score * 1
+        else:
+            combined_scores[id] += score * 1
+    for id, score in title_scores:
+        if id not in combined_scores:
+            combined_scores[id] = score * scale
+        else:
+            combined_scores[id] += score * scale
 
-    # Convert search sentence to numerical vector
-    search_vector = vectorizer.transform([search_sentence])
+    keys_to_delete = [
+        id for id in combined_scores if combined_scores[id] < (1 + 5) * threshold
+    ]
+    [combined_scores.pop(id) for id in keys_to_delete]
 
-    # Calculate cosine similarity between search sentence and all posts
-    similarities = np.dot(post_matrix, search_vector.T).toarray().flatten()
+    # Sort dictionary by value (score) in descending order
+    sorted_scores = sorted(combined_scores.items(), key=lambda x: x[1], reverse=True)
 
-    # Sort similarities in descending order and get indices of top n posts
-    if n > len(posts):
-        top_n_indices = similarities.argsort()[::-1]
-    else:
-        top_n_indices = similarities.argsort()[::-1][:n]
-
-    # Retrieve the ids of the top n posts
-    top_n_ids = [posts[i][0] for i in top_n_indices if similarities[i] > threshold]
-
-    # Return the ids of the top n posts
-    return top_n_ids
+    # Extract sorted list of ids
+    sorted_ids = [item[0] for item in sorted_scores]
+    return sorted_ids
