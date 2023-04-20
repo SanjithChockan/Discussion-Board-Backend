d6a8da58ab7303dd7a4e52816004c0b42ca38b94
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-04-20 10:35:23 -0500

    fixing student model error

--- main/routes/read_other.py
+++ main/routes/read_other.py
@@ -7,12 +7,14 @@ read_other_bp = Blueprint("read_other", __name__)
 
 DEFAULT_N = 50
 
+
 # Get answers for specific post
 @read_other_bp.route("/get_answers_for_post/<int:post_id>", methods=["GET"])
 @read_other_bp.route("/get_answers_for_post/<int:post_id>/<int:n>", methods=["GET"])
 def get_answers_for_post(post_id, n=DEFAULT_N):
     answers = Answer.query.filter_by(post_id=post_id).limit(n).all()
     return jsonify([answer.serialize() for answer in answers]), 200
+
 
 # Get course
 @read_other_bp.route("/get_course/<int:course_id>", methods=["GET"])
@@ -22,3 +24,6 @@ def get_course(course_id):
         return jsonify(course.serialize()), 200
     else:
         return jsonify({"message": "Course not found"}), 404
+
+
+# Get user
