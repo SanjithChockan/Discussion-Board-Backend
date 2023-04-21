70d818e731da279a02bb64956d8456f448efe220
Sanjith Chockan <sanjith.chockan@gmail.com>
2023-04-21 14:09:20 -0500

    added get_replies for a parent asnwer

--- main/routes/read_other.py
+++ main/routes/read_other.py
@@ -10,6 +10,35 @@ read_other_bp = Blueprint("read_other", __name__)
 DEFAULT_N = 50
 
 
+# Get replies from of an answer
+@read_other_bp.route("/get_answers_for_post/<int:post_id>/<int:answer_id>", methods=["GET"])
+def get_replies_from_answer(post_id, answer_id):
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
+
+    query = text(
+        """
+        WITH RECURSIVE nested_answers AS (
+        SELECT *, 1 as depth, CAST(answer_id AS CHAR(255)) as path
+        FROM answers
+        WHERE post_id = :post_id AND parent_answer IS answer_id
+
+        UNION ALL
+
+        SELECT a.*, depth + 1, CONCAT(path, ',', a.answer_id)
+        FROM answers AS a
+        INNER JOIN nested_answers AS na ON a.parent_answer = na.answer_id
+        WHERE a.post_id = :post_id
+        )
+
+        SELECT * FROM nested_answers ORDER BY path;
+        """
+    )
+
+    answers = session.query(Answer).from_statement(query).params(post_id=post_id, answer_id=answer_id).all()
+
+    return jsonify([answer.serialize() for answer in answers]), 200
+
 # Get answers for specific post
 @read_other_bp.route("/get_answers_for_post/<int:post_id>", methods=["GET"])
 @read_other_bp.route("/get_answers_for_post/<int:post_id>/<int:n>", methods=["GET"])
