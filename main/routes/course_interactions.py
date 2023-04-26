948763b2d91a3d5ee01c86676b0040a6c37a3d08
Sameer Haider <43320143+sameer-haider@users.noreply.github.com>
2023-04-26 09:17:29 -0500

    raise error if not student

--- main/routes/course_interactions.py
+++ main/routes/course_interactions.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 from ..models import *
 from util import rule_based, gpt_api
 from datetime import datetime
+from errors import *
 
 courses_bp = Blueprint("courses", __name__)
 
@@ -17,6 +18,11 @@ courses_bp = Blueprint("courses", __name__)
 @courses_bp.route("/user_courses", methods=["GET"])
 @jwt_required()
 def user_courses():
-    registrations = Registration.query.filter_by(student_id=get_jwt_identity()).all()
+    user_id = get_jwt_identity()
+    student = Student.query.get(user_id)
+    if not student:
+        raise BadRequestError("User is not a student")
+
+    registrations = Registration.query.filter_by(student_id=user_id).all()
     courses = [registration.course.serialize() for registration in registrations]
     return jsonify(courses), 200
